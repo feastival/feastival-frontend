aad5f16acb627669de6560da524ce6a6916500b9
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-27 20:09:43 +0700

    styling sidebar

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Card from '../components/Card';
 import Table from '@/components/Explore/Table';
 import Head from 'next/head';
+import Sidebar from '@/components/Explore/Sidebar';
 
 export default function ExploreRoute() {
   return (
@@ -10,6 +11,7 @@ export default function ExploreRoute() {
       <Head>
         <title>Explore</title>
       </Head>
+      <Sidebar />
       <Card />
       <Artists />
       <Table />
