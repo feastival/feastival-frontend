4be4e1f71c7452ff781cf027d5a9149d4bd31fb4
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-28 10:59:21 +0700

    move explore route to folder

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,9 +1,10 @@
 import Artists from '@/components/Explore/Artists';
 import React from 'react';
-import Card from '../components/Card';
+
 import Table from '@/components/Explore/Table';
 import Head from 'next/head';
 import Sidebar from '@/components/Explore/Sidebar';
+import Card from '@/components/Card';
 
 export default function ExploreRoute() {
   return (
