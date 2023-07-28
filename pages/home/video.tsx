a60d5f6009e1311bee5474705b964a6f8eb0fee6
anggr <anggorookky@gmail.com>
2023-07-28 09:49:58 +0800

    style: Fix formatting and indentation in home/trending.tsx and home/video.tsx

--- pages/home/video.tsx
+++ pages/home/video.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default function VideoSection() {
   return (
@@ -8,13 +8,20 @@ export default function VideoSection() {
           the white shoes and couples company
         </div>
         <div className="self-stretch text-center text-neutral-500 text-lg font-normal leading-[27px]">
-          Kolaborasi The White Shoes And Couples Company bersama Together Whatever Session.
+          White Shoes And Couples Company pada gelaran Sychronize Fest 2018.
         </div>
       </div>
       <div className="w-[1300px] h-[633px] bg-zinc-300 rounded-xl mx-auto my-4">
-      <iframe width="1300" height="633" src="https://www.youtube.com/embed/QVaQhJxHJuQ" title="YouTube video player" frameBorder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        </div>
+        <iframe
+          width="1300"
+          height="633"
+          src="https://www.youtube.com/embed/cej8-8z2b6M"
+          title="YouTube video player"
+          frameBorder="2"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
-
