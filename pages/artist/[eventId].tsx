5840addc3d42fdab056a553fe98906acfcb0f721
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-29 02:16:05 +0700

    add main flow

--- pages/artist/[eventId].tsx
+++ pages/artist/[eventId].tsx
@@ -5,11 +5,24 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import { Event } from '@/lib/eventsInterface';
 import { API_URL } from '@/lib/api';
+import { getCookie } from 'cookies-next';
 
-export default function ArtistRoute() {
+export default function ArtistRouteById() {
   const router = useRouter();
   const { eventId } = router.query;
-  const [event, setEvent] = useState<Event[]>([]);
+  const [event, setEvent] = useState<any>([]);
+  const [artists, setArtists] = useState<any>([]);
+  const token = getCookie('token');
+  const dateOptionsHour = {
+    hour: '2-digit' as const,
+    minute: '2-digit' as const,
+    hour12: false,
+  };
+  const dateOptionsDay = {
+    day: '2-digit' as const,
+    month: 'long' as const,
+    year: 'numeric' as const,
+  };
 
   const fetchEvent = async () => {
     if (eventId) {
@@ -17,9 +30,28 @@ export default function ArtistRoute() {
         // Fetch event data by its ID from the API using Axios
         const response = await axios.get(`${API_URL}/events/${eventId}`);
         setEvent(response.data);
+        setArtists(response.data.artists);
       } catch (error) {
         console.error('Error fetching event data:', error);
       }
+    }
+  };
+
+  const formattedTime = new Date(event.startedAt);
+
+  const handleSaveEvent = async (id: string | string[] | undefined) => {
+    try {
+      await axios.post(
+        `${API_URL}/user/me/track-event`,
+        {
+          eventId: id,
+        },
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+
+      alert('Save Event Success!!!');
+    } catch (error) {
+      alert('Error saving event data');
     }
   };
 
@@ -33,6 +65,7 @@ export default function ArtistRoute() {
   }
 
   console.log(event);
+  console.log(artists);
   const events = [
     {
       date: '13 Agustus 2023',
@@ -53,7 +86,7 @@ export default function ArtistRoute() {
             <div className="md:w-[1440px] relative mx-auto mb-4 bg-black md:mb-0">
               <div className="px-4 lg:px-0">
                 <h1 className="mb-5 text-[60px] font-semibold leading-tight text-center text-white font-bebasNeue">
-                  Chvrches: Here With Me
+                  {event.name}
                 </h1>
               </div>
               <img
@@ -67,7 +100,10 @@ export default function ArtistRoute() {
               <div className="text-5xl font-bebasNeue leading-[64px] text-[#2e2e2e] mt-px shrink-0">
                 Event Detail
               </div>
-              <Button className="bg-[#9747ff] hover:bg-purple-900 self-start flex flex-col justify-center h-12 px-6 mt-4 rounded-xl">
+              <Button
+                onClick={() => handleSaveEvent(eventId)}
+                className="bg-[#9747ff] hover:bg-purple-900 self-start flex flex-col justify-center h-12 px-6 mt-4 rounded-xl"
+              >
                 <div className="whitespace-nowrap font-poppins leading-[24px] text-white">
                   Remind This Event
                 </div>
@@ -99,16 +135,34 @@ export default function ArtistRoute() {
                     </tr>
                   </thead>
                   <tbody>
-                    {events.map((event, index) => (
-                      <tr key={index} className="text-base bg-white">
-                        <td className="px-6 py-2 ">{event.date}</td>
-                        <td className="px-6 py-2 ">{event.time}</td>
-                        <td className="px-6 py-2 ">{event.location}</td>
-                        <td className="px-6 py-2 ">{event.venue}</td>
-                        <td className="px-6 py-2 ">{event.category}</td>
-                        <td className="px-6 py-2 ">{event.organizer}</td>
-                      </tr>
-                    ))}
+                    <tr key={event.id} className="text-base bg-white">
+                      <td className="px-6 py-2 ">
+                        {formattedTime
+                          ? formattedTime.toLocaleString(
+                              'id-ID',
+                              dateOptionsDay,
+                            )
+                          : 'Sedang di update..'}
+                      </td>
+                      <td className="px-6 py-2 ">
+                        {formattedTime
+                          ? formattedTime.toLocaleString(
+                              'id-ID',
+                              dateOptionsHour,
+                            )
+                          : 'Sedang di update..'}
+                      </td>
+                      <td className="px-6 py-2 ">{event.location}</td>
+                      <td className="px-6 py-2 ">{event.venue}</td>
+                      <td className="px-6 py-2 ">
+                        {event.category ? event.category : 'Sedang di update..'}
+                      </td>
+                      <td className="px-6 py-2 ">
+                        {event.organizer
+                          ? event.organizer
+                          : 'Sedang di update..'}
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
@@ -116,12 +170,7 @@ export default function ArtistRoute() {
             <div className="md:w-[1000px] pb-10 md:pb-20 mt-12 w-full flex items-center justify-center mx-auto">
               <div className="items-start text-lg leading-relaxed text-gray-700 md:mx-auto justisfy-center">
                 <p className="mx-auto leading-[32px] pr-0 md:pr-20">
-                  Advantage old had otherwise sincerity dependent additions. It
-                  in adapted natural hastily is justice. Six draw you him full
-                  not mean evil. Prepare garrets it expense windows shewing do
-                  an.s She projection advantages resolution son indulgence. Part
-                  sure on no long life am at ever. In songs above he as drawn
-                  to.
+                  {event.description}
                 </p>
               </div>
               <div className="m-auto mb-30 md:w-1/3">
@@ -136,7 +185,7 @@ export default function ArtistRoute() {
                     />
                     <div>
                       <p className="py-1 text-sm font-semibold text-gray-700">
-                        Chvrches
+                        {''}
                       </p>
                       <p className="text-xs font-semibold text-gray-600">
                         {' '}
