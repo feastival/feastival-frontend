2e4a6d4a1044a85dc206f09a32801ef87ce3b3ca
anggr <anggorookky@gmail.com>
2023-07-28 23:10:16 +0800

    feat(Sidebar): add click functionality to sidebar items

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,21 +1,31 @@
 import Artists from '@/components/Explore/Artists';
-import React from 'react';
-
+import React, { useState } from 'react';
 import Table from '@/components/Explore/Table';
 import Head from 'next/head';
 import Sidebar from '@/components/Explore/Sidebar';
 import Card from '@/components/Card';
 
 export default function ExploreRoute() {
+  const [selected, setSelected] = useState('Event');
+
+  const handleClick = (selection: string) => {
+    setSelected(selection);
+  };
+
   return (
     <div className="mt-32">
       <Head>
         <title>Explore</title>
       </Head>
-      <Sidebar />
-      <Card />
-      <Artists />
-      <Table />
+      <Sidebar handleClick={handleClick} />
+      {selected === 'Event' ? (
+        <>
+          <Card />
+          <Table />
+        </>
+      ) : (
+        <Artists />
+      )}
     </div>
   );
 }
