762c32c7074cf775e18be835adceab1df291bdbc
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-30 11:31:00 +0700

    add artist profile page

--- pages/artist/index.tsx
+++ pages/artist/index.tsx
@@ -9,30 +9,7 @@ import { API_URL } from '@/lib/api';
 export default function ArtistRoute() {
   const router = useRouter();
   const { eventId } = router.query;
-  const [event, setEvent] = useState<Event[]>([]);
 
-  const fetchEvent = async () => {
-    if (eventId) {
-      try {
-        // Fetch event data by its ID from the API using Axios
-        const response = await axios.get(`${API_URL}/events/${eventId}`);
-        setEvent(response.data);
-      } catch (error) {
-        console.error('Error fetching event data:', error);
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchEvent();
-  }, [eventId]);
-
-  if (!event) {
-    // If the event is not found or the data is still loading, you can handle the case accordingly (e.g., show a loading state or an error message).
-    return <div>Loading...</div>;
-  }
-
-  console.log(event);
   const events = [
     {
       date: '13 Agustus 2023',
@@ -46,110 +23,38 @@ export default function ArtistRoute() {
     },
   ];
   return (
-    <div className="pt-32 bg-black">
+    <div className="pt-32">
       <div className="mx-auto">
-        <main className="mt-10">
-          <div className="w-full pb-10 bg-black">
-            <div className="md:w-[1440px] relative mx-auto mb-4 bg-black md:mb-0">
-              <div className="px-4 lg:px-0">
-                <h1 className="mb-5 text-[60px] font-semibold leading-tight text-center text-white font-bebasNeue">
-                  Chvrches: Here With Me
-                </h1>
-              </div>
-              <img
-                src="https://res.cloudinary.com/djudfrj8s/image/upload/v1688051265/week-20/2018-11-06-chvrches-live-music-hall-koeln_027_rj6wim.jpg"
-                className="object-cover w-[1000px] rounded-2xl shadow-sm shadow-slate-500 lg:rounded mx-auto"
-              />
-            </div>
-          </div>
-          <div className="w-full pt-5 bg-white">
-            <div className="md:w-[1000px] md-px-0 px-2 w-full flex flex-inline justify-between mx-auto mb-6">
-              <div className="text-5xl font-bebasNeue leading-[64px] text-[#2e2e2e] mt-px shrink-0">
-                Event Detail
-              </div>
-              <Button className="bg-[#9747ff] hover:bg-purple-900 self-start flex flex-col justify-center h-12 px-6 mt-4 rounded-xl">
-                <div className="whitespace-nowrap font-poppins leading-[24px] text-white">
-                  Remind This Event
-                </div>
-              </Button>
-            </div>
-            <div className="flex justify-center mt-10 font-poppins">
-              <div className="relative overflow-x-auto sm:rounded-lg font-poppins">
-                <table className="w-full text-sm text-left dark:text-gray-400">
-                  <thead className="text-2xl font-bold ">
-                    <tr>
-                      <th scope="col" className="px-6">
-                        Date
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Time
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Location
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Venue
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Category
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Organizer
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {events.map((event, index) => (
-                      <tr key={index} className="text-base bg-white">
-                        <td className="px-6 py-2 ">{event.date}</td>
-                        <td className="px-6 py-2 ">{event.time}</td>
-                        <td className="px-6 py-2 ">{event.location}</td>
-                        <td className="px-6 py-2 ">{event.venue}</td>
-                        <td className="px-6 py-2 ">{event.category}</td>
-                        <td className="px-6 py-2 ">{event.organizer}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-            <div className="md:w-[1000px] pb-10 md:pb-20 mt-12 w-full flex items-center justify-center mx-auto">
-              <div className="items-start text-lg leading-relaxed text-gray-700 md:mx-auto justisfy-center">
-                <p className="mx-auto leading-[32px] pr-0 md:pr-20">
-                  Advantage old had otherwise sincerity dependent additions. It
-                  in adapted natural hastily is justice. Six draw you him full
-                  not mean evil. Prepare garrets it expense windows shewing do
-                  an.s She projection advantages resolution son indulgence. Part
-                  sure on no long life am at ever. In songs above he as drawn
-                  to.
-                </p>
-              </div>
-              <div className="m-auto mb-30 md:w-1/3">
-                <div className="p-4">
-                  <p className="py-1 text-sm font-semibold text-gray-700">
-                    Lineup
-                  </p>
-                  <div className="flex py-2">
-                    <img
-                      src="https://res.cloudinary.com/djudfrj8s/image/upload/v1688051265/week-20/2018-11-06-chvrches-live-music-hall-koeln_027_rj6wim.jpg"
-                      className="object-cover w-12 h-12 mr-2 rounded-full"
-                    />
-                    <div>
-                      <p className="py-1 text-sm font-semibold text-gray-700">
-                        Chvrches
-                      </p>
-                      <p className="text-xs font-semibold text-gray-600">
-                        {' '}
-                        Band{' '}
-                      </p>
-                      <p className="py-1 text-sm font-semibold text-gray-700"></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
+        <h2 className="font-bold text-5xl text-center">NIKI</h2>
+        <div className="flex flex-wrap justify-center items-center mb-14 mt-8">
+          <img
+            src="https://res.cloudinary.com/djudfrj8s/image/upload/v1690547059/feastival/wp7368797_fkmyhv.jpg"
+            alt="artist-photo"
+            className="w-full p-4 object-cover xl:w-[30rem] xl:h-[25rem] md:w-1/2 md:order-1 mb-5 md:mb-0 rounded-3xl"
+          />
+          <p className="w-full md:w-1/2 md:order-2 md:pl-6 p-4">
+            Nicole Zefanya (born 24 January 1999), known professionally as NIKI,
+            is an Indonesian singer, songwriter, and record producer. She is
+            currently based in the United States and signed with the record
+            label 88rising. At 15, she was the opening act for Taylor Swift's
+            The Red Tour in Jakarta after winning a competition arranged by
+            Swift and the ice cream brand Wall's, called Ride to Fame. Zefanya
+            maintained a YouTube channel throughout her teenage years, on which
+            she uploaded original songs and covers, which were eventually
+            released on Spotify under the name Nicole Zefanya. In 2017, Zefanya
+            moved to Nashville, Tennessee to study music at Lipscomb University.
+            She released two singles, "See U Never" and "I Like U" under the
+            American mass media company 88rising as an artist under their label.
+            She released the song "Vintage" in May 2018 as the first single for
+            her debut extended play, Zephyr. On April 24, 2019, Zefanya released
+            a visualizer of her single titled "lowkey" from her album Nicole,
+            which would become one of 88rising's most popular videos uploaded on
+            its Youtube channel. In September 2020, Zefanya released her debut
+            album, Moonchild. In December 2020, Zefanya released "Hallway
+            Weather", a holiday single. On 12 August 2022, Zefanya's second
+            album, Nicole, was released.
+          </p>
+        </div>
       </div>
     </div>
   );
