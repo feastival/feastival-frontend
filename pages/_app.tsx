37671617fffa3c26303a3321c2cef27f0e8ce573
meselghea <122159476+meselghea@users.noreply.github.com>
2023-08-02 17:41:18 +0700

    change head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
+import Head from 'next/head';
 
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -12,6 +13,21 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+     <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha512-..."
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <title>Feastival</title>
+        <meta
+          name="description"
+          content="FIND AMAZING EVENT
+NEAR YOU"
+        />
+      </Head>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
         <Navbar />
