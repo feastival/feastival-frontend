fa8bd5dfa7ea91ba31470cf2660efca8ce76e062
anggr <anggorookky@gmail.com>
2023-08-02 10:43:37 +0800

    style: redesign detail event

--- components/EventById/Countdown.tsx
+++ components/EventById/Countdown.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 interface CountdownProps {
   date: string;
@@ -44,26 +44,30 @@ const Countdown: React.FC<CountdownProps> = ({ date }) => {
     return () => clearInterval(timer);
   }, [calculateTimeLeft]);
 
-  return (
-    <div className="flex font-poppins">
-      <div className="flex gap-3 sm:gap-1 flex-row bg-black h-36 rounded-2xl overflow-hidden pt-5 pr-3 sm:pt-0 sm:pr-0">
-        {Object.keys(timeLeft).map((key) => (
-          <div key={key} className="flex flex-col bg-black p-8 sm:w-32 w-16">
-            <div className="h-16 sm:h-20 bg-[#2A303C]">
-              <div className="h-[60px] flex justify-center bg-black sm:text-3xl text-xl text-white">
-                {timeLeft[key]}
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <span className="text-lg sm:text-2xl text-center text-white">
-                {timeLeft[key] === 1 ? key.slice(0, -1) : key}
-              </span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  const formatTimeLeft = (timeLeft: TimeLeft) => {
+    const intervals = ['days', 'hours', 'minutes', 'seconds'];
+    let duration = '';
+
+    intervals.forEach((interval, index) => {
+      if (!timeLeft[interval]) {
+        return;
+      }
+
+      if (index === 0 || !timeLeft[intervals[index - 1]]) {
+        duration +=
+          timeLeft[interval] +
+          ' ' +
+          (timeLeft[interval] === 1 ? interval.slice(0, -1) : interval) +
+          ' ';
+      }
+    });
+
+    return duration.trim();
+  };
+
+  const duration = formatTimeLeft(timeLeft);
+
+  return duration ? <span>{duration}</span> : null;
 };
 
 export default Countdown;
