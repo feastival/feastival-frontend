96361caaa11542478782e58e42afecafc8589ebf
anggr <anggorookky@gmail.com>
2023-08-02 12:05:21 +0800

    refactor: Simplify calculateTimeLeft function and formatTimeLeft function

--- components/EventById/Countdown.tsx
+++ components/EventById/Countdown.tsx
@@ -5,31 +5,68 @@ interface CountdownProps {
 }
 
 interface TimeLeft {
-  [key: string]: number | undefined;
-  days?: number;
-  hours?: number;
-  minutes?: number;
-  seconds?: number;
+  [key: string]: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  months: number;
 }
 
 const Countdown: React.FC<CountdownProps> = ({ date }) => {
   const calculateTimeLeft = useCallback((): TimeLeft => {
-    let year = new Date(date);
-    let difference = +year - +new Date();
+    let targetDate = new Date(date);
+    let currentDate = new Date();
+
     let timeLeft: TimeLeft = {
+      years: 0,
+      months: 0,
+      weeks: 0,
       days: 0,
       hours: 0,
       minutes: 0,
       seconds: 0,
     };
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
+    if (targetDate > currentDate) {
+      // Calculate difference in years
+      let yearsDifference =
+        targetDate.getFullYear() - currentDate.getFullYear();
+      timeLeft.years = yearsDifference;
+
+      // Calculate difference in months
+      timeLeft.months = yearsDifference * 12;
+      timeLeft.months -= currentDate.getMonth();
+      timeLeft.months += targetDate.getMonth();
+      if (targetDate.getDate() < currentDate.getDate()) {
+        timeLeft.months--;
+      }
+
+      // Calculate difference in weeks and days
+      let copyCurrentDate = new Date(currentDate.getTime());
+      if (targetDate.getDate() < currentDate.getDate()) {
+        copyCurrentDate.setMonth(copyCurrentDate.getMonth() + 1);
+      }
+      copyCurrentDate.setMonth(copyCurrentDate.getMonth() + timeLeft.months);
+      timeLeft.days = Math.floor(
+        (targetDate.getTime() - copyCurrentDate.getTime()) /
+          (1000 * 60 * 60 * 24),
+      );
+      timeLeft.weeks = Math.floor(timeLeft.days / 7);
+      timeLeft.days %= 7;
+
+      // Calculate difference in hours, minutes, and seconds
+      timeLeft.hours = Math.floor(
+        ((targetDate.getTime() - copyCurrentDate.getTime()) /
+          (1000 * 60 * 60)) %
+          24,
+      );
+      timeLeft.minutes = Math.floor(
+        ((targetDate.getTime() - copyCurrentDate.getTime()) / (1000 * 60)) % 60,
+      );
+      timeLeft.seconds = Math.floor(
+        ((targetDate.getTime() - copyCurrentDate.getTime()) / 1000) % 60,
+      );
     }
 
     return timeLeft;
@@ -45,24 +82,28 @@ const Countdown: React.FC<CountdownProps> = ({ date }) => {
   }, [calculateTimeLeft]);
 
   const formatTimeLeft = (timeLeft: TimeLeft) => {
-    const intervals = ['days', 'hours', 'minutes', 'seconds'];
+    const intervals = [
+      'years',
+      'months',
+      'weeks',
+      'days',
+      'hours',
+      'minutes',
+      'seconds',
+    ];
     let duration = '';
 
-    intervals.forEach((interval, index) => {
-      if (!timeLeft[interval]) {
-        return;
+    for (let i = 0; i < intervals.length; i++) {
+      const interval = intervals[i];
+      if (timeLeft[interval]) {
+        duration = `${timeLeft[interval]} ${
+          timeLeft[interval] === 1 ? interval.slice(0, -1) : interval
+        }`;
+        break;
       }
+    }
 
-      if (index === 0 || !timeLeft[intervals[index - 1]]) {
-        duration +=
-          timeLeft[interval] +
-          ' ' +
-          (timeLeft[interval] === 1 ? interval.slice(0, -1) : interval) +
-          ' ';
-      }
-    });
-
-    return duration.trim();
+    return duration;
   };
 
   const duration = formatTimeLeft(timeLeft);
