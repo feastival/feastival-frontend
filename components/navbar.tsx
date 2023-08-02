5ce0b52dd635842a558029ce3e5e2a18e15e5f92
meselghea <122159476+meselghea@users.noreply.github.com>
2023-08-03 06:54:05 +0700

    update login and readonly searchbar in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -89,7 +89,7 @@ export default function Navbar() {
            {isLoggedIn ? (
             <button
               onClick={handleLogout} // Add the logout function here (implement handleLogout)
-              className="ml-4 gap-2 items-center justify-center mx-auto lg:flex bg-red-500 text-white font-poppins rounded-xl hover:bg-red-900 w-[199px] h-[50px] hidden"
+              className="ml-4 gap-2 items-center justify-center mx-auto lg:flex bg-red-500 text-white font-poppins rounded-xl hover:bg-red-900 w-[120px] h-[50px] hidden"
             >
               Logout
             </button>
@@ -174,9 +174,9 @@ export default function Navbar() {
               {isLoggedIn ? (
               <button
                 onClick={handleLogout} // Add the logout function here (implement handleLogout)
-                className="ml-4 gap-2 items-center justify-center mx-auto lg:flex bg-red-500 text-white font-poppins rounded-xl hover:bg-red-900 w-[199px] h-[50px] hidden"
+                className="items-center justify-center w-12 h-12 gap-2 mx-auto text-white bg-red-500 rounded-full lg:flex font-poppins hover:bg-red-900"
               >
-                Logout
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="logout"><path d="M12.59,13l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H3a1,1,0,0,0,0,2ZM12,2A10,10,0,0,0,3,7.55a1,1,0,0,0,1.8.9A8,8,0,1,1,12,20a7.93,7.93,0,0,1-7.16-4.45,1,1,0,0,0-1.8.9A10,10,0,1,0,12,2Z"></path></svg>
               </button>
             ) : (
               <Link href="/auth/login"
